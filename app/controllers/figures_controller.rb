f90f0527c2855ab09d13c05e8d52577d1c8ed221
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-28 22:30:35 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -3,6 +3,9 @@ class FiguresController < ApplicationController
 
   post '/figures' do
     binding.pry
+    figure = Figure.create(name: params[:figure][:name])
+    titles = params[:figure][:title_ids]
+    titles.map! {|title| Title.find(title)}
 
     redirect "/figures/#{figure.id}"
   end
