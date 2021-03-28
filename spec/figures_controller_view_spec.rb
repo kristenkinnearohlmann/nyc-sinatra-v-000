b09cce582945dd16b8f818843ea87358a3568735
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-28 23:45:34 +0000

    Automatically backed up by Learn

--- spec/figures_controller_view_spec.rb
+++ spec/figures_controller_view_spec.rb
@@ -40,7 +40,6 @@ describe FiguresController do
   it "allows you to create a new figure with a landmark" do
     visit '/figures/new'
     fill_in :figure_name, :with => "Doctor Who"
-    binding.pry
     check "landmark_#{Landmark.last.id}"
     click_button "Create New Figure"
     figure = Figure.last
