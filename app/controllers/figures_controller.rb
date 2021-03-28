d8836e79e0eacf400535800212f6dcc52ee878d9
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-28 23:58:44 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -4,18 +4,18 @@ class FiguresController < ApplicationController
   post '/figures' do
 
     figure = Figure.create(name: params[:figure][:name])
-
-    # create title objects from selected titles
-    titles = params[:figure][:title_ids]
-    titles.map! {|title| Title.find(title)}
-    figure.titles << titles
 binding.pry
-    # create landmark objects from selected landmarks
-    landmarks = params[:figure][:landmark_ids]
-    landmarks.map! {|landmark| Landmark.find(landmarks)}
-    figure.landmarks << landmarks
-
-    figure.save
+#     # create title objects from selected titles
+#     titles = params[:figure][:title_ids]
+#     titles.map! {|title| Title.find(title)}
+#     figure.titles << titles
+# binding.pry
+#     # create landmark objects from selected landmarks
+#     landmarks = params[:figure][:landmark_ids]
+#     landmarks.map! {|landmark| Landmark.find(landmarks)}
+#     figure.landmarks << landmarks
+#
+#     figure.save
 
     redirect "/figures/#{figure.id}"
   end
