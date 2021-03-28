bbc893932eb64db0942fa41f000f9a9ee4193112
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-28 22:12:40 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -2,6 +2,8 @@ class FiguresController < ApplicationController
   # add controller methods
 
   get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
 
     erb :'figures/new'
   end
