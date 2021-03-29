b4313309bde78fc52662d1b9e409a0db7cbaf97f
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-29 00:17:18 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -9,18 +9,6 @@ class FiguresController < ApplicationController
     # creating with the hash, so I was doing it the longer way. But doing so meant that the associations
     # were not being made as expected. LESSON LEARNED!!
     figure = Figure.create(params[:figure])
-binding.pry
-#     # create title objects from selected titles
-#     titles = params[:figure][:title_ids]
-#     titles.map! {|title| Title.find(title)}
-#     figure.titles << titles
-# binding.pry
-#     # create landmark objects from selected landmarks
-#     landmarks = params[:figure][:landmark_ids]
-#     landmarks.map! {|landmark| Landmark.find(landmarks)}
-#     figure.landmarks << landmarks
-#
-#     figure.save
 
     redirect "/figures/#{figure.id}"
   end
