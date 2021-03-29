baf5a6b13e495a994e4935a046f66bd6f7c764a4
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-29 00:39:16 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -16,7 +16,7 @@ class FiguresController < ApplicationController
     if !params[:landmark][:name].empty?
       figure.landmarks << Landmark.create(params[:landmark])
     end
-binding.pry
+
     figure.save
     redirect "/figures/#{figure.id}"
   end
