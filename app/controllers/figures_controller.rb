822c6ef0b08547d5f31211628d39ee1eae5c609f
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-29 00:17:06 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -2,7 +2,12 @@ class FiguresController < ApplicationController
   # add controller methods
 
   post '/figures' do
-
+    # Worked with Matthew and looked at student and main solutions and finally determined the issue
+    # For the test with landmarks, title isn't passed and landmark is
+    # Since I was trying to create my code to individual work with titles and landmarks, rather than
+    # creating via the params hash, it failed. I hadn't had success, nor any tests failed me, on
+    # creating with the hash, so I was doing it the longer way. But doing so meant that the associations
+    # were not being made as expected. LESSON LEARNED!!
     figure = Figure.create(params[:figure])
 binding.pry
 #     # create title objects from selected titles
