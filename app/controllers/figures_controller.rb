13e4c040d79742cc854fe3f62bb1fb036b126fb2
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-29 01:13:21 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -50,7 +50,7 @@ class FiguresController < ApplicationController
   patch '/figures/:id' do
     figure = Figure.find(params[:id])
     figure.name = params[:figure][:name]
-    binding.pry
+
     if !params[:landmark].empty?
       figure.landmarks << Landmark.create(params[:landmark])
     end
